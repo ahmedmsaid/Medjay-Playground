cbeefac591fce5578a6b03f18201c35e80554296
ahmedmsaid <ahmedmsaid307@gmail.com>
2023-10-25 18:46:45 +0300

    security issues sloved

--- src/app/weather-api.service.ts
+++ src/app/weather-api.service.ts
@@ -8,7 +8,6 @@ import { environment } from 'src/environments/environment.development';
 export class WeatherAPIService {
   city: string = 'Shibīn al Kawm'
   units: string = 'metric'
-  //apiUrl: string = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=${this.units}`
   
   constructor(private http: HttpClient) { }
 
