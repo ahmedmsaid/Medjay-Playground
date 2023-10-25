b0b369393769f900221a3edca1a93ea44d81f9ff
ahmedmsaid <ahmedmsaid307@gmail.com>
2023-10-25 18:44:14 +0300

    solved some security issues

--- src/app/weather-api.service.ts
+++ src/app/weather-api.service.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherAPIService {
   city: string = 'Shibīn al Kawm'
-  apiKey: string = 'a8ef68136c1c408c86a496bf3253eb3d'
   units: string = 'metric'
   //apiUrl: string = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=${this.units}`
   
   constructor(private http: HttpClient) { }
 
   getWeather(city: string = this.city) {
-    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=${this.units}`)
+    return this.http.get(`${environment.apiUrl}?q=${city}&appid=${environment.apiKey}&units=${this.units}`)
   }
 }
